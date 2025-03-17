323503c2199113ead65e90214e69241a820d91ec
coolnik0805@gmail.com <neeraj.work.official@gmail.com>
2025-03-17 15:05:37 +0530

    Updated changes

--- script.js
+++ script.js
@@ -5,11 +5,19 @@ let ctrlIcon = document.getElementById('ctrlIcon');
 
 const songs = [
     { src: "Media/Aawara - (Raag.Fm).mp3", img: "Media/Aawara_King,_Zaeden,KSHMR.webp", title: "Aawara", artist: "King, Zaeden, KSHMR" },
-    { src: "Media/Sarkaare - (Raag.Fm).mp3", img: "Media/Sarkaare.png", title: "Sarkaare", artist: "King" },
+    { src: "Media/Sarkaare - (Raag.Fm).mp3", img: "Media/Sarkaare.jpeg", title: "Sarkaare", artist: "King" },
     { src: "Media/Stay - (Raag.Fm).mp3", img: "Media/Stay.jpg", title: "Stay", artist: "King" },
-    { src: "Media/STILL THE SAME - (Raag.Fm).mp3", img: "Media/Way_Bigger.webp", title: "Still The Same", artist: "King" }
-];
+    { src: "Media/STILL THE SAME - (Raag.Fm).mp3", img: "Media/Way_Bigger.webp", title: "Still The Same", artist: "King" },
+    { src: "Media/Tu Jaana Na - (Raag.Fm).mp3", img: "Media/Tu_Jaana_Na.jpeg", title: "Tu Jaana Na", artist: "King" },
+    { src: "Media/Dhoondein Sitaare - (Raag.Fm).mp3", img: "Media/Dhoondein_Sitaare.jpg", title: "Dhoonde Sitaare", artist: "King ,Astha Gill"},
+    { src: "Media/Tu Aake Dekhle - (Raag.Fm).mp3", img: "Media/Tu_aake_dekhle.jpg", title: "Tu Aake Dekhle", artist: "King"},
+    { src: "Media/Dracula - (Raag.Fm).mp3", img: "Media/Dracula.jpg", title: "Dracula", artist: "King"},
+    { src: "Media/Ilzaam - (Raag.Fm).mp3", img: "Media/Ilzaam.jpg", title: "Ilzaam", artist: "King, Arjun Kanungo"},
+    { src: "Media/Way Bigger - (Raag.Fm).mp3", img:"Media/Way_Bigger.webp", title: "Way_Bigger", artist: "King"},
+    { src: "Media/She Dont Give A - (Raag.Fm).mp3", img:"Media/She_Dont_give_a.jpg", title: "She_Dont_Give_a", artist: "King"},
 
+    
+];
 let currentSongIndex = 0;
 
 // Play / Pause Function
